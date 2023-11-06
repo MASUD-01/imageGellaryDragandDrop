a2ca7b79f3441fe14631f61a3ceeb9acfe678127
MASUD-01 <masudrana96141@gmail.com>
2023-11-06 21:29:57 +0600

    finsih the imageGallery task

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,113 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from "react";
 import {
-  GridContextProvider,
-  GridDropZone,
-  GridItem,
-  swap,
-} from "react-grid-dnd";
-import "./App.css";
-import Images from "./api/images.json";
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  UniqueIdentifier,
+  closestCenter,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+import { imageList } from "./imageList";
+import Header from "./Header";
+import { SortableItem } from "./SortableItem";
+import { Photo } from "./Photo";
 
-interface IImages {
-  id: number;
-  image: string;
-}
+const App = () => {
+  const [items, setItems] = useState(imageList);
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const [selected, setSelected] = useState<string[]>([]);
 
-function App() {
-  const [items, setItems] = useState<IImages[]>(Images);
-  const [selectItem, setSelectItem] = useState<number[]>([]);
+  console.log(selected);
 
-  function onChange(_: string, sourceIndex: number, targetIndex: number) {
-    const nextState = swap(items, sourceIndex, targetIndex);
-    setItems(nextState);
-  }
-  /* --this function is set select item and deselect item */
-  const handleTrackItems = (e: any, id: number) => {
-    if (e?.target.checked && !selectItem.includes(id)) {
-      setSelectItem([...selectItem, id]);
-    } else {
-      const removeItem = selectItem.filter((item) => item != id);
-      setSelectItem(removeItem);
-    }
+  const handleDeleteClick = () => {
+    setItems((prevItems) => prevItems.filter((url) => !selected.includes(url)));
+    setSelected([]);
   };
 
-  /* ---this function is remove selected item from state */
-  const handleDeleteItems = () => {
-    const deleteItem = items.filter((item) => !selectItem.includes(item.id));
-    setItems(deleteItem);
-    setSelectItem([]);
+  const clearSelect = () => {
+    setSelected([]);
   };
-  console.log(selectItem);
+
   return (
-    <>
-      <GridContextProvider onChange={onChange}>
-        <div className="flex justify-center items-center  bg-[#f1f3f4]">
-          <div className="w-[900px] h-[800px] bg-white p-2">
-            {selectItem.length ? (
-              <div className="flex justify-between ">
-                <p>
-                  <input
-                    type="checkbox"
-                    readOnly
-                    checked={!!selectItem.length}
-                  />{" "}
-                  {selectItem.length} File{selectItem.length > 1 ? "s" : ""}{" "}
-                  Selected
-                </p>
-
-                <button
-                  className="text-red-500 hover:underline"
-                  onClick={() => handleDeleteItems()}
-                >
-                  Delete file{selectItem.length > 1 ? "s" : ""}
-                </button>
-              </div>
-            ) : (
-              <></>
-            )}
-            <GridDropZone
-              id="items"
-              boxesPerRow={4}
-              rowHeight={225}
-              style={{
-                height: "700px",
-                width: "900px",
-              }}
-            >
-              {items?.map((item: IImages) => (
-                <GridItem
-                  key={item.id}
-                  className="border-2 rounded-md "
-                  style={{ height: "200px", width: "200px" }}
-                >
-                  {item.id == 12 ? (
-                    <div
-                      className={`h-full flex justify-center items-center ${
-                        item.id == 12 && "active:bg-violet-700"
-                      } `}
-                    >
-                      <p className="text-center"> add image</p>
-                    </div>
-                  ) : (
-                    <div className="relative group flex justify-center ">
-                      <img
-                        src={item.image}
-                        alt={`image/${item.id}`}
-                        style={{ height: "200px", width: "200px" }}
-                      />
-                      <div
-                        className={`absolute top-0 hover:cursor-grab opacity-[.2] w-full h-full ${
-                          selectItem.includes(item.id)
-                            ? "bg-black"
-                            : "group-hover:bg-black"
-                        }`}
-                      ></div>
-                      <input
-                        type="checkbox"
-                        className={`w-5 h-5 absolute top-0 group-hover:block ${
-                          !selectItem.includes(item.id) && "hidden"
-                        }`}
-                        onClick={(e) => handleTrackItems(e, item.id)}
-                      />
-                    </div>
-                  )}
-                </GridItem>
-              ))}
-            </GridDropZone>
+    <div className="container mx-auto">
+      <Header
+        total={selected.length}
+        handleDeleteClick={handleDeleteClick}
+        clearSelect={clearSelect}
+      />
+      <DndContext
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        onDragCancel={handleDragCancel}
+      >
+        <SortableContext items={items} strategy={rectSortingStrategy}>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: `repeat(5, 1fr)`,
+              gridGap: 10,
+              padding: 10,
+            }}
+          >
+            {items.map((url, index) => (
+              <SortableItem
+                key={url}
+                url={url}
+                index={index}
+                selected={selected}
+              />
+            ))}
           </div>
-        </div>
-      </GridContextProvider>
-    </>
+        </SortableContext>
+
+        <DragOverlay adjustScale={true}>
+          {activeId ? (
+            <Photo url={activeId} index={items.indexOf(String(activeId))} />
+          ) : null}
+        </DragOverlay>
+      </DndContext>
+    </div>
   );
-}
+
+  function handleDragStart(event: DragStartEvent) {
+    setActiveId(event.active.id);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(String(active.id));
+        const newIndex = items.indexOf(String(over?.id));
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    } else {
+      // Check if the photo is already selected
+      const isSelected = selected.includes(String(active.id));
+
+      if (isSelected) {
+        setSelected((prevSelected) =>
+          prevSelected.filter((id) => id !== active.id)
+        );
+      } else {
+        setSelected((prevSelected) => [...prevSelected, String(active.id)]);
+      }
+    }
+
+    setActiveId(null);
+  }
+
+  function handleDragCancel() {
+    setActiveId(null);
+  }
+};
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,40 @@
+interface Props {
+  total: number;
+  handleDeleteClick: () => void;
+  clearSelect: () => void;
+}
+
+const Header = ({ total, handleDeleteClick, clearSelect }: Props) => {
+  return (
+    <div className="p-3 border-b text-black font-semibold">
+      <div className="flex justify-between items-center">
+        <div>
+          {total === 0 && <div className="font-bold">Gallery</div>}
+          {total > 0 && (
+            <div className="flex gap-4">
+              <div className="font-semibold">{total} Files Selected</div>
+              <button
+                className="border border-green-500 text-xs px-1 rounded hover:bg-green-600 hover:text-white transition-all duration-150"
+                onClick={clearSelect}
+              >
+                Reset
+              </button>
+            </div>
+          )}
+        </div>
+        <div>
+          {total > 0 && (
+            <button
+              onClick={handleDeleteClick}
+              className="text-red-500 font-semibold"
+            >
+              Delete Files
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
